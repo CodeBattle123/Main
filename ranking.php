a4460ec3067d1d450dc926a5c51dc6ac25414ecd
Bloodlister <AsenJ.Mihaylov@gmail.com>
2016-11-03 18:37:25 +0200

    Made ranking.html/css
    What remains to be done is to make the description elements responsive.

---
+++ ranking.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Ranking</title>
+      <link rel="stylesheet" href="styles/headerAndFooter.css">
+      <link rel="stylesheet" href="styles/ranking.css">
+   </head>
+   <body>
+      <?php include_once('header.html'); ?>
+
+      <main>
+
+         <div class="desc">
+            <h2>Top ranking!</h2>
+         </div>
+
+         <div class="ranking-container">
+            <div class="desc_user">
+               <div class="desc_left">
+                  <h4 class="desc-name">Name:</h4>
+                  <h4 class="desc_exp">Exp:</h4>
+               </div>
+
+               <div class="desc_right">
+                  <h4 class="desc_clan">Clan:</h4>
+                  <p class="desc_rank">Rank:</p>
+               </div>
+            </div>
+      <!---Asen - people go down from here --->
+
+            <div class="user">
+               <img src="images/footer_github.png"   class="avatar"/>
+               <div class="left">
+                  <h4 class="name">Lord Bolton</h4>
+                  <h4 class="exp">Exp: 999999</h4>
+               </div>
+
+               <div class="right">
+                  <h4 class="clan">Scrubs</h4>
+                  <p class="rank">Rank:&nbsp;<b>420</b></p>
+               </div>
+            </div>
+
+            <div class="user">
+               <img src="images/footer_github.png"   class="avatar"/>
+               <div class="left">
+                  <h4 class="name">Lord Bolton</h4>
+                  <h4 class="exp">Exp: 999999</h4>
+               </div>
+
+               <div class="right">
+                  <h4 class="clan">Scrubs</h4>
+                  <p class="rank">Rank:&nbsp;<b>420</b></p>
+               </div>
+            </div>
+
+            <div class="user">
+               <img src="images/footer_github.png"   class="avatar"/>
+               <div class="left">
+                  <h4 class="name">Lord Bolton</h4>
+                  <h4 class="exp">Exp: 999999</h4>
+               </div>
+
+               <div class="right">
+                  <h4 class="clan">Scrubs</h4>
+                  <p class="rank">Rank:&nbsp;<b>420</b></p>
+               </div>
+            </div>
+
+            <div class="user">
+               <img src="images/footer_github.png"   class="avatar"/>
+               <div class="left">
+                  <h4 class="name">Lord Bolton</h4>
+                  <h4 class="exp">Exp: 999999</h4>
+               </div>
+
+               <div class="right">
+                  <h4 class="clan">Scrubs</h4>
+                  <p class="rank">Rank:&nbsp;<b>420</b></p>
+               </div>
+            </div>
+
+
+         </div>
+
+      </main>
+
+      <?php include_once('footer.html'); ?>
+   </body>
+</html>
