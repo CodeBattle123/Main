30a1336cfee3d72d0d23e52e6d557d0d8fc7dd7d
NikolaNikolov <whitestarhs@gmail.com>
2016-11-08 16:29:55 +0200

    Fixed something

--- AboutUs.php
+++ AboutUs.php
@@ -28,7 +28,7 @@
     <h1 align="center" style="font-family: MainText">About the project</h1><hr>
     <div style="width: 1050px; padding: 15px;">
         Lorem ipsum dolor sit amet, omittam repudiare at sed. Te eum sumo soleat feugait, eum te audire scribentur, scripta numquam oportere ea nec. Sit quas omnium invenire no, lorem doming graecis nec id, animal salutatus deterruisset cum id. Est cu utinam docendi inimicus. Sed no veniam diceret intellegam.
-        Eu quas eleifend laboramus mea, ei option quaeque dissentias pro. Porro integre ne ius, habeo verear vel ex, qui eligendi torquatos no. Sint inciderint sit cu, ea has meis legendos. Eu usu etiam voluptaria, et usu idque libris. Ei mei movet mucius.
+        Eu quas eleifend laboramus mea, ei option  quaeque dissentias pro. Porro integre ne ius, habeo verear vel ex, qui eligendi torquatos no. Sint inciderint sit cu, ea has meis legendos. Eu usu etiam voluptaria, et usu idque libris. Ei mei movet mucius.
         Ei postea labitur mel, dictas petentium disputationi ad sea. Sit cibo dicant deseruisse cu, id summo viderer aliquando vis. Vel no malorum euismod, voluptatibus concludaturque te vix. Modo vidit intellegam his an.
         Ea ius suas bonorum, id nec illum affert integre. Sit melius assueverit ea. No fugit atqui salutandi sea. Has te affert graecis percipit, sale alienum mea te. Ei vim diam tibique, duo in altera aeterno scripserit. At clita tritani pro, ne homero timeam nominavi vel.
         Ea verear pericula instructior cum. In vel impedit salutatus abhorreant, alienum reprehendunt usu ei. Eu usu adhuc reque eirmod. No sed probo vocent veritus, deleniti splendide sadipscing ex nam. Cu iudico appellantur sit, mei et meis tincidunt consequuntur.
