e9a9689b47e8a30ace2f6db2b12249da642d807e
NikolaNikolov <whitestarhs@gmail.com>
2016-11-11 14:02:16 +0200

    .

---
+++ battlesequence.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-16">
+    <title>Battle sequence</title>
+    <link rel="stylesheet" type="text/css" href="styles/headerAndFooter.css">
+    <link rel="stylesheet" type="text/css" href="styles/battlesequence.css">
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
+    <link rel="icon" type="image/png" href="images/icon.png"/></head>
+
+<body>
+<?php include 'header.html'?>
+<?php include 'sidebar.html'?>
+
+<h1 class="title" 20px;">Battle sequence</h1><hr>
+
+<div class="battle-screen">
+
+</div>
+
+<?php include 'footer.html'?>
+</body>
+</html>
