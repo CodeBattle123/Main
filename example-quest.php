ef2150291418a287d0136afa1680125be709a2b3
Bloodlister <AsenJ.Mihaylov@gmail.com>
2016-11-17 16:43:52 +0200

    Major changes to the example quest.
    Example quest page changes:
     - Automatically set width of input fields.
     - Whole redesign of the page.
    
    Header and footer css:
     - Changed fonts.

--- example-quest.php
+++ example-quest.php
@@ -3,34 +3,48 @@
 <head>
     <link rel="stylesheet" href="styles/headerAndFooter.css">
     <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
-    <link rel="stylesheet" href="styles/example-quest-style.css">
     <link rel="stylesheet" href="styles/sidebar.css">
     <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/example-quest-style.css">
     <meta charset="UTF-8">
-    <title>quests</title>
+    <title>Quests</title>
+
 </head>
 <body>
 <?php include 'header.php' ?>
 <?php include 'sidebar.html' ?>
-<div class="wrapper">
-    <h1 class="langName">Test Your Ruby Skills</h1>
-    <h3 class="level">Level 1/10</h3>
 
+<div class="wrapper">
     <div class="questContainer">
-            <h4 class="objective">Fill Blanks to Print Numbers 1 to 10 </h4>
-            <div id="counter"></div>
+        <h1 class="langName">Test Your <span>Ruby</span> Skills</h1>
+        <h3 class="level">Level 1/10</h3>
+        <hr>
+        <h4 class="objective">Fill Blanks to Print Numbers 1 to 10</h4>
+
+        <div class="codeContainer">
             <pre class="quest">
                 <span>i=1</span><!-- <span>&lt;irb&gt;</span> -->
-                <input type="text" id="ex-quest-input-1" class="input" style="width: 60px" ><span>i<11</span></input>
-                <input type="text" id="ex-quest-input-2" class="input" style="width: 60px" cols="3"><span>"i: ", i ,"\n"</span></input>
-                <span>i</span><input type="text" id="ex-quest-input-3" class="input" style="width: 60px" ><span>1</span></input>
+                <input type="text" id="ex-quest-input-1" class="input" maxlength="5"><span> i&lt;11 </span></input>
+                    <input type="text" id="ex-quest-input-2" class="input" maxlength="5"><span>"i: ", i ,"\n"</span></input>
+                    <span>i</span><input type="text" id="ex-quest-input-3" class="input" maxlength="2"><span>1</span></input>
                 <span>end</span><!-- <span>&lt;/irb&gt;</span> -->
             </pre>
 
-            <input type="button" id="btn-submit-quest-example" value="check"> <br>
-            Result: <span id="result"></span>
+            <div id="counter">
+                <br><input type="button" class="checkButton" id="btn-submit-quest-example" value="check">
+                <span id="result">Result: </span>
+            </div>
+        </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    let inputs = document.getElementsByClassName('input');
+
+    for(item of inputs) {
+        item.style.width = (item.maxLength * 11) + 'px';
+    }
+</script>
 
 <script>
     function countdown() {
@@ -38,7 +52,8 @@
         function tick() {
             var counter = document.getElementById("counter");
             seconds--;
-            counter.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds);
+            counter.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds)
+                +"<br><input type='button' class='checkButton' id='btn-submit-quest-example' value='check'> <br> <span id='result'>Result: </span>";
             if (seconds > 0) {
                 let t = setTimeout(tick, 1000);
                 $('#btn-submit-quest-example').click(function () {
