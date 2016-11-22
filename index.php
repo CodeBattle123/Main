f4703cd5cee635bd031b895737ab7abb9027d6be
NikolaNikolov <whitestarhs@gmail.com>
2016-11-22 15:49:51 +0200

    Fixed the buttons in AboutUs.php and edited the main page design.

--- index.php
+++ index.php
@@ -15,19 +15,16 @@
 
 <div class="wrapper">
     <div class="home-banner"></div>
-    <div class="content">
+        <div class="content">
 
         <div class="right">
-            <h1>About us</h1><hr>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consectetur corporis dicta magni nihil omnis possimus quibusdam quisquam sed. Atque consectetur exercitationem nemo nulla officiis quae, quis quisquam similique sin. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aspernatur aut consequatur, deleniti eligendi enim et expedita illo in itaque laboriosam molestias necessitatibus nihil provident rem sequi velit vitae voluptas.
-                Lorem ipsum dolor sit amet, omittam repudiare at sed. Te eum sumo soleat feugait, eum te audire scribentur, scripta numquam oportere ea nec. Sit quas omnium invenire no, lorem doming graecis nec id, animal salutatus deterruisset cum id. Est cu utinam docendi inimicus. Sed no veniam diceret intellegam.
-                Eu quas epericulaleifend laboramus mea, ei option quaeque dissentias pro. Porro integre ne ius, habeo verear vel ex, qui eligendi torquatos no. Sint inciderint sit cu, ea has meis legendos. Eu usu etiam voluptaria, et usu idque libris. Ei mei movet mucius.
-            </p>
+            <a href="#" id="btn-home-join">Join the battle</a>
+            <div class="scr"></div>
         </div>
 
         <div class="left">
-            <h1>About us</h1><hr>
+
+            <h1>About the project</h1><hr>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consectetur corporis dicta magni nihil omnis possimus quibusdam quisquam sed. Atque consectetur exercitationem nemo nulla officiis quae, quis quisquam similique sin. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aspernatur aut consequatur, deleniti eligendi enim et expedita illo in itaque laboriosam molestias necessitatibus nihil provident rem sequi velit vitae voluptas.
                 Lorem ipsum dolor sit amet, omittam repudiare at sed. Te eum sumo soleat feugait, eum te audire scribentur, scripta numquam oportere ea nec. Sit quas omnium invenire no, lorem doming graecis nec id, animal salutatus deterruisset cum id. Est cu utinam docendi inimicus. Sed no veniam diceret intellegam.
