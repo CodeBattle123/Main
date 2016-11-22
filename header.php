9e6d2fd5ebfddfa615cbc4a29292ed7358f79b8f
Bloodlister <AsenJ.Mihaylov@gmail.com>
2016-11-22 17:13:49 +0200

    Made it so that you can't load the page in Internet Explorer

--- header.php
+++ header.php
@@ -1,4 +1,26 @@
 <?php
+
+	function get_browser_name($user_agent)
+	{
+	if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
+	elseif (strpos($user_agent, 'Edge')) return 'Edge';
+	elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
+	elseif (strpos($user_agent, 'Safari')) return 'Safari';
+	elseif (strpos($user_agent, 'Firefox')) return 'Firefox';
+	elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'Internet Explorer';
+
+	return 'Other';
+	}
+
+// Usage:
+	$browserCheck = get_browser_name($_SERVER['HTTP_USER_AGENT']);
+
+	if ($browserCheck == "Internet Explorer") {
+		echo "Please get a different browser.";
+		exit;
+	}
+
+
     include_once('db/connect.php');
 ?>
 
