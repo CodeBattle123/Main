33f958eed0d37e3adf8211840d684c328584d5e5
antonia100 <antonya100yanova@gmail.com>
2016-11-29 15:35:41 +0200

    quest check script

---
+++ scripts/questCheckRuby.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function checkQuest(inputArr, resultArr){
+
+    let resultPoints = ;
+
+    for(let i=0; i<resultPoints; i++){
+        if(inputArr[i] == resultArr[i]){
+
+        }
+    }
+}
