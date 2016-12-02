f9f6afa6b7c626628e2b88ad8409da182b45451b
mila <k1464355@kcl.ac.uk>
2016-12-02 17:59:46 +0200

    added functionality to create clan, upload picture and search clan

--- clanPage.php
+++ clanPage.php
@@ -15,7 +15,19 @@
          <div class="clanAvatarHolder"></div>
 
          <div class="clanInfo">
-            <h3 class="clanName">BadBoyz</h3>
+            <?php
+            $user_name = $_SESSION ['username'];
+
+            $sql = "SELECT * FROM users WHERE nickname = '$user_name'";
+
+            $query = mysqli_query($connect, $sql);
+
+            $row = $query->fetch_assoc();
+
+            echo '<h1>' . $row['clan'] . '</h1>';
+
+            ?>
+            <h3 class="clanName">Behf</h3>
             <h3 class="clanDesc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
 
             <ul class="members">
