f44c026c94b98668172d66b9a0ca1c54dcb5ae89
mila <k1464355@kcl.ac.uk>
2016-12-13 17:13:45 +0200

    contact us page

--- contactus.php
+++ contactus.php
@@ -13,7 +13,7 @@
 <?php include 'header.php'?>
 
 <div class="wrapper">
-    <form class="form">
+    <form class="form" action="contactus.php" method="post" enctype="multipart/form-data" >
         <p class="name">
             <label class="inputLabel" for="name">Name</label><br>
             <input type="text" name="name" id="name" placeholder="Your full name" />
@@ -34,8 +34,54 @@
         </p>
 
         <p class="submit">
-            <input type="submit" value="Send" />
+            <input type="submit" name="submit" value="Send" />
         </p>
+
+        <?php
+
+            if (isset($_POST['submit'])){
+                $validate = true;
+
+                $email_to="you@gmail.com";
+
+                $name = $_POST['name'];
+                if (strlen($name) == 0) {
+                    echo "<li class=\"message\">You must enter your name.</li>";
+                    $validate = false;
+                }
+
+                $email_from = preg_replace('#[^a-z0-9]#i', '', $_POST['email']);
+                if (strlen($email_from) < 1) {
+                    echo '<li class="message">Email must be inputed.</li>';
+                    $validate = false;
+                }
+
+                $subject = $_POST['subject'];
+                if (strlen($subject) == 0) {
+                    echo "<li class=\"message\">You must enter a subject.</li>";
+                    $validate = false;
+                }
+
+                $text = $_POST['text'];
+                if (strlen($text) == 0) {
+                    echo "<li class=\"message\">You must enter a message.</li>";
+                    $validate = false;
+                }
+
+                if ($validate==true){
+
+                    $headers = 'From: ' . $email_from . "\r\n".
+                        'Reply-To: ' . $email_from ."\r\n" .
+                        'X-Mailer: PHP/' . phpversion();
+
+
+                    mail($email_to, $subject, $text, $headers);
+
+                    echo "<li class=\"message\">Thank you for contacting us. We will be in touch with you soon.</li>";
+                }
+            }
+        ?>
+
     </form>
 
     <iframe  frameborder="0" class="map" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJtxmnpMyFqkAR11jn2Lz8U98&key=AIzaSyAveHIKy7pEHie0CkXQRzdsRQXE4tvenSg" allowfullscreen>
