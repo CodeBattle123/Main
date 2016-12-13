dde6a4a4a18077e5d3f2dc18fc1fdd8efc69fcda
NikolaNikolov <whitestarhs@gmail.com>
2016-12-13 18:38:40 +0200

    Fixed the make clan page

--- scripts/clan_edit.php
+++ scripts/clan_edit.php
@@ -8,7 +8,14 @@ if (isset($_POST['description'])) {
     $description = $_POST['description'];
     $sql = "UPDATE teams SET description='$description' WHERE name ='$log_clan'";
     $query = mysqli_query($connect, $sql);
+    header("location: ../clan_edit.php");
 }
 
+if (isset($_POST['deleteclan'])) {
+    mysqli_query($connect, "UPDATE users SET clan='' WHERE clan='$log_clan'");
+    $sql = "DELETE FROM teams WHERE name ='$log_clan'";
+    $query = mysqli_query($connect, $sql);
+    header("location: ../make-clan.php");
+}
 
 ?>
--- scripts/removeFromClan.php
+++ scripts/removeFromClan.php
@@ -6,20 +6,17 @@ include '../db/login_status.php';
 echo $log_clan . "<br>";
 $user_name = $_POST['username'];
 
-
-
-
 if (isset($_POST['remove'])){
     $sql = "UPDATE users SET clan = '' WHERE nickname = '$user_name'";
     mysqli_query($connect, $sql);
-    header("location: ../clanPage.php");
+    header("location: ../clan_edit.php");
 }
 
 
 if (isset($_POST['makeleader'])){
     $sql = "UPDATE teams SET leader = '$user_name' WHERE name = '$log_clan'";
     mysqli_query($connect, $sql);
-    header("location: ../clanPage.php");
+    header("location: ../clan_edit.php");
 }
 
 ?>
